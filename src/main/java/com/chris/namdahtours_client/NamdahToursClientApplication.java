9ebdf3bba2792039b29f519ed51948c9ef275f29
Christopher Lijoodi <christopherlijoodi@Power.local>
2022-08-16 16:56:24 +0300

    client

---
+++ src/main/java/com/chris/namdahtours_client/NamdahToursClientApplication.java
@@ -0,0 +1,13 @@
+package com.chris.namdahtours_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamdahToursClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamdahToursClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chris/namdahtours_client/NamdahToursClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.namdahtours_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamdahToursClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
